b0607cec014664d17b6dac3a32a814a3c9be769b
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-06-21 04:30:21 +0900

    :bento: Update scratchpad

--- scratchpad/flow_ok.py
+++ scratchpad/flow_ok.py
@@ -1,17 +1,21 @@
 """Compute the flow."""
 
+from __future__ import annotations
+
+import networkx as nx
 from fastflow import flow
 
-# graph with flow and gflow
-# 0 - 2 - 4
-#     |
+# Flow exists
 # 1 - 3 - 5
+#     |
+# 2 - 4 - 6
 #
-# in: 0, 1
-# out: 4, 5
+# in: 1, 2
+# out: 5, 6
 
-g = [{2}, {3}, {0, 3, 4}, {1, 2, 5}, {2}, {3}]
-iset = {0, 1}
-oset = {4, 5}
+e: list[tuple[int, int]] = [(1, 3), (3, 5), (2, 4), (4, 6), (3, 4)]
+g = nx.Graph(e)
+iset = {1, 2}
+oset = {5, 6}
 
 print(flow.find(g, iset, oset))
