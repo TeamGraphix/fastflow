d0dbe75282ec05555ab146d1589b4e123806b344
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-07-26 07:15:35 +0900

    :memo: Update docs

--- src/common.rs
+++ src/common.rs
@@ -2,7 +2,16 @@
 
 use std::collections::BTreeSet;
 
+/// Set of nodes indexed by 0-based integers.
 pub type Nodes = hashbrown::HashSet<usize>;
-pub type OrderedNodes = BTreeSet<usize>;
+/// Simple graph encoded as list of neighbors.
 pub type Graph = Vec<Nodes>;
+/// Layer representation of the flow partial order.
 pub type Layer = Vec<usize>;
+
+/// Ordered set of nodes.
+///
+/// # Note
+///
+/// Used only when iteration order matters.
+pub(crate) type OrderedNodes = BTreeSet<usize>;
