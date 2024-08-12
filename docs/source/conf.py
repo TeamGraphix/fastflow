48a1a3450ae6ff44b746843d65466e2b7151f2df
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-08-12 19:31:21 +0900

    :art: Use V

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,30 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'fastflow'
-copyright = '2024, S.S.'
-author = 'S.S.'
-release = '0.1.0'
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../python/fastflow"))
+
+project = "fastflow"
+copyright = "2024, TeamGraphix"
+author = "S.S."
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
