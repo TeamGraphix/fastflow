828d067295de9769b756a9e89bea117b6d2a23a5
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-08-15 15:29:47 +0900

    :wrench: Update doc config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,8 @@ release = "0.1.0"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
 ]
 
 templates_path = ["_templates"]
@@ -33,5 +35,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
