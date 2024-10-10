543c0828dfcca186d87230b2f543c47eedb74073
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-10-10 10:23:59 +0900

    :lipstick: Use furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,6 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    "sphinx_rtd_theme",
 ]
 
 templates_path = ["_templates"]
@@ -37,5 +36,5 @@ templates_path = ["_templates"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 # html_static_path = []
