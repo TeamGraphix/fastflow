dcb1ba377a3b012e742c509814067490ab76054c
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-06-10 04:42:37 +0900

    :bento: Update solver example

--- examples/solver.py
+++ examples/solver.py
@@ -1,31 +1,46 @@
 """Example code for GF(2) solver."""
 
+# %%
 from __future__ import annotations
 
 import numpy as np
 from fastflow import solver
 
+# %%
 # Coefficient matrix
 a = [
     [1, 1],
     [0, 0],
 ]
 
-# Right-hand sides packed as column vectors
+# Two right-hand sides packed as columns
 #
 # No. 0: [0, 0]
 # No. 1: [1, 1]
-b = [
+bm = [
     [0, 1],
     [0, 1],
 ]
 
-x0, x1 = solver.solve(a, b)
+# %%
+x0, x1 = solver.solve(a, bm)
 
-# If any, solutions are returned as column vectors.
-#   sum(x0) is guaranteed to be minimal.
+# Solution found for bm[:, 0]
 assert x0 is not None
+
+# If singular, pick an arbitrary solution
 assert np.array_equal(x0, [0, 0])
 
-# Otherwise, None is returned.
+# No solution for bm[:, 1]
 assert x1 is None
+
+# %%
+
+# One right-hand side as a vector
+bv = [1, 0]
+
+# %%
+(x,) = solver.solve(a, bv)
+
+assert x is not None
+assert np.array_equal(x, [1, 0])
