c77f927b56006c7b16ccc27b295187523a754931
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-06-09 20:19:12 +0900

    :rotating_light: Fix mypy warnings
    Only seen in old pythons

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -19,6 +19,7 @@ def test_1d() -> None:
     a = np.asarray([[1, 1], [0, 1]])
     b = np.asarray([0, 1])
     (x,) = solver.solve(a, b)
+    assert x is not None
     np.testing.assert_array_equal(x, [1, 1])
 
 
@@ -26,6 +27,8 @@ def test_2d() -> None:
     a = np.asarray([[1, 1], [0, 1]])
     b = np.asarray([[0, 1], [1, 1]])
     x0, x1 = solver.solve(a, b)
+    assert x0 is not None
+    assert x1 is not None
     np.testing.assert_array_equal(x0, [1, 1])
     np.testing.assert_array_equal(x1, [0, 1])
 
@@ -61,13 +64,11 @@ def test_inconsistent_rows() -> None:
 def test_bad_type() -> None:
     a = np.asarray([[1, 1], [0, 1]], dtype=np.bool_)
 
-    b = [1.0, 1.0]
     with pytest.raises(TypeError, match=r".*non-integral.*"):
-        solver.solve(a, b)
+        solver.solve(a, [1.0, 1.0])
 
-    b = np.asarray([1, 2])
     with pytest.raises(ValueError, match=r".*not equivalent.*"):
-        solver.solve(a, b)
+        solver.solve(a, [1, 2])
 
 
 @pytest.mark.parametrize(
